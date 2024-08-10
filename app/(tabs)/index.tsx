f9071b4da8b6ccc510adbad1d62b321530046da0
Alexis Bejarano <aletzisbejaranov@hotmail.com>
2024-08-10 09:59:06 -0700

    Se termino diseño de tabla principal

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,20 +1,18 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { DataTable } from 'react-native-paper';
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 
+import tw from 'twrnc'; // Import TailWind CSS.
 
-import tw from 'twrnc'; //Import TailWind CSS.
-//style={tw``}
-
-//Saldo Total
+// Saldo Total
 const saldoTotal = "15,000";
 const montoSaldoTotal = "$" + saldoTotal;
 const moreSaldoTotal = "Banco + Billetera";
 
-//Saldo Real
+// Saldo Real
 const totalReal = "20,000";
 const montoTotalReal = "$" + totalReal;
 const moreTotalReal = "Monto Total";
@@ -22,25 +20,57 @@ const moreTotalReal = "Monto Total";
 export default function HomeScreen() {
   return (
     <ThemedView style={styles.titleContainer}>
-        <ThemedView style={tw`flex-row justify-center w-full text-center`}>
-          <ThemedView style={tw`mx-8`}>
-            <ThemedText type="subtitle">Saldo Total</ThemedText>
+      <ThemedView style={tw`flex items-center justify-center w-full text-center`}>
+        <ThemedView style={tw`flex flex-row content-center`}>
+          <ThemedView style={tw`mx-8 w-35 h-35`}>
+            <ThemedText style={tw`text-center`} type="subtitle">Saldo Total</ThemedText>
             <ThemedText style={tw`text-center`} type="default">{montoSaldoTotal}</ThemedText>
             <ThemedText style={tw`text-center`} type="default">{moreSaldoTotal}</ThemedText>
           </ThemedView>
-          <ThemedView style={tw`mx-8`}>
-            <ThemedText type="subtitle">Total Real</ThemedText>
+          <ThemedView style={tw`mx-8 w-35 h-35`}>
+            <ThemedText style={tw`text-center`} type="subtitle">Total Real</ThemedText>
             <ThemedText style={tw`text-center`} type="default">{montoTotalReal}</ThemedText>
             <ThemedText style={tw`text-center`} type="default">{moreTotalReal}</ThemedText>
           </ThemedView>
         </ThemedView>
 
-      <ThemedView style={tw`mt-10`}>
-        <ThemedText type='subtitle'>Aqui van las tablas</ThemedText>
+        <ThemedView style={tw`mx-8 w-35 h-35 my-auto mx-auto`}>
+          <ThemedText style={tw`text-center`} type="subtitle">Banco</ThemedText>
+          <ThemedText style={tw`text-center`} type="subtitle">BBVA</ThemedText>
+          <ThemedText style={tw`text-center`} type="subtitle">Salario Actual</ThemedText>
+          <ThemedText style={tw`text-center`} type="default">{montoSaldoTotal}</ThemedText>
+          <ThemedText style={tw`text-center`} type="default">{moreSaldoTotal}</ThemedText>
+        </ThemedView>
+
+        <DataTable>
+          <DataTable.Header>
+            <DataTable.Title style={styles.movimientosColumnTop}>Movimientos</DataTable.Title>
+            <DataTable.Title style={styles.resumenColumnTop}>Resumen</DataTable.Title>
+            <DataTable.Title style={styles.fechaColumn}>Fecha</DataTable.Title>
+          </DataTable.Header>
+
+          <DataTable.Row>
+            <DataTable.Cell>2000</DataTable.Cell>
+            <DataTable.Cell style={styles.resumenColumn}>Pago de renta</DataTable.Cell>
+            <DataTable.Cell style={styles.fechaColumn}>05/24/2024</DataTable.Cell>
+          </DataTable.Row>
+
+          <DataTable.Row>
+            <DataTable.Cell>100</DataTable.Cell>
+            <DataTable.Cell style={styles.resumenColumn}>asdasdasd</DataTable.Cell>
+            <DataTable.Cell style={styles.fechaColumn}>25/03/2024</DataTable.Cell>
+          </DataTable.Row>
+
+          <DataTable.Pagination
+            page={1}
+            numberOfPages={3}
+            onPageChange={page => {
+              console.log(page);
+            }}
+            label="1-2 of 6"
+          />
+        </DataTable>
       </ThemedView>
-
-
-
     </ThemedView>
   );
 }
@@ -48,18 +78,28 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   titleContainer: {
     flexDirection: 'row',
-    alignItems: 'center',
     gap: 8,
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  movimientosColumnTop: {
+    flex: 1, // Movimientos será más estrecho
+    justifyContent: 'center',
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  movimientosColumn: {
+    flex: 1, // Movimientos será más estrecho
+    justifyContent: 'center',
+  },
+  resumenColumn: {
+    flex: 3, // Resumen será más ancho
+  },
+  resumenColumnTop: {
+    flex: 3, // Resumen será más ancho
+    justifyContent: 'center',
+  },
+  fechaColumn: {
+    flex: 1, // Fecha también será más estrecho
+    justifyContent: 'center',
   },
 });
+
+
+
